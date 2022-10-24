f108e70831e2b34437bba752a47a616304ec0351
PenHsuanWang <willy118116@gmail.com>
2022-10-24 11:08:38 +0800

    add artifact server container run independently

--- build_mlflow_image/startup_script.sh
+++ build_mlflow_image/startup_script.sh
@@ -2,8 +2,12 @@
 
 set -m
 
-mlflow server --backend-store-uri mysql+pymysql://'mlflow':'mlflow'@db:3306/mlflowruns --default-artifact-root ./mlruns --host 0.0.0.0 --port 8080 &
+sudo mlflow server --artifacts-destination ./mlartifacts --gunicorn-opts "--log-level debug" --serve-artifacts --artifacts-only --host 0.0.0.0 --port 5500 &
 
-mlflow ui --backend-store-uri mysql+pymysql://'mlflow':'mlflow'@db:3306/mlflowruns --default-artifact-root ./mlruns --host 0.0.0.0 --port 5000
+sudo mlflow server --backend-store-uri mysql+pymysql://'mlflow':'mlflow'@db:3306/mlflowruns --default-artifact-root http://127.0.0.1:5500/api/2.0/mlflow-artifacts/artifacts/experiments --gunicorn-opts "--log-level debug" --host 0.0.0.0 --port 5000
+
+
+
+# mlflow ui --backend-store-uri mysql+pymysql://'mlflow':'mlflow'@db:3306/mlflowruns --default-artifact-root ./mlruns --host 0.0.0.0 --port 5000
 
 fg %1
